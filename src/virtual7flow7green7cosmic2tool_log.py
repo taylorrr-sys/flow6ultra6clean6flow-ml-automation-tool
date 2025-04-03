1d57804f8d1f2ea8cb1eff39167733867a899741
taylorrr-sys <taylorrolyat999@gmail.com>
2025-04-04 04:10:05 +0900

    Update development log at 2025-04-04 04:10:04

--- src/virtual7flow7green7cosmic2tool_log.py
+++ src/virtual7flow7green7cosmic2tool_log.py
@@ -12,3 +12,19 @@ def bubble_sort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
+
+# Update at 2025-04-04 04:10:04
+# Optimized the algorithm
+# Refactored the code
+
+
+from PIL import Image
+
+def resize_image(image_path, size):
+    with Image.open(image_path) as img:
+        resized = img.resize(size)
+        return resized
+
+def convert_to_grayscale(image_path):
+    with Image.open(image_path) as img:
+        return img.convert('L')
